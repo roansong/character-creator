2b9ec775f7e19b53930273412f4e6a82384290ef
Roan Song <roansong@gmail.com>
2021-11-07 11:42:26 +0200

    Submission for the character creator task

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 
 import CharacterEditor from './components/CharacterEditor';
 import Footer from './components/Footer';
+import Highlight from './components/Highlight';
 
 function App() {
   return (
     <>
+      <Highlight />
       <CharacterEditor />
       <Footer />
     </>
---
+++ src/components/Highlight/Highlight.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import styles from './Highlight.module.css';
+
+const Highlight = () => {
+  return (
+    <div className={styles.highlight}></div>
+  );
+};
+
+export default Highlight;
